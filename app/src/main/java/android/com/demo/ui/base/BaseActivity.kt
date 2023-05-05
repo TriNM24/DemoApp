c58c3b426aacb61831ca408bd64b18f5369a4fa7
tri.nguyenminh10 <tri.nguyenminh10@vib.com.vn>
2023-05-05 09:41:25 +0700

    add firebase analytic https://firebase.google.com/docs/analytics/screenviews?authuser=1

--- app/src/main/java/android/com/demo/ui/base/BaseActivity.kt
+++ app/src/main/java/android/com/demo/ui/base/BaseActivity.kt
@@ -6,15 +6,25 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 abstract class BaseActivity<BD : ViewDataBinding>: AppCompatActivity() {
     abstract val resourceLayoutId : Int
     var binding: BD? = null
 
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     abstract fun onInitView(root: View?)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
+        //Firebase Analytics will auto log screen view event
+
         binding = DataBindingUtil.setContentView(this, resourceLayoutId)
         setContentView(binding?.root)
         onInitView(binding?.root)
